2a7e37274aa48d8c281ad4b1d020ccd2f1c882d3
ClemDuss <dussoll.clem@gmail.com>
2020-03-11 22:05:18 +0100

    rooms ended

--- src/app/view/home/rooms/rooms.component.ts
+++ src/app/view/home/rooms/rooms.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+interface roomsFormat {
+  nomVille: string;
+  nbSalles: number;
+}
+
+const rooms: roomsFormat[] = [
+  {nomVille: 'Annecy', nbSalles: 4},
+  {nomVille: 'Chamonix', nbSalles: 1},
+  {nomVille: 'Thonon-les-Bains', nbSalles: 1},
+  {nomVille: 'Bonneville', nbSalles: 1},
+]
+
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
@@ -7,6 +19,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomsComponent implements OnInit {
   public title:string = 'Salles';
+
+  public roomsList: roomsFormat[] = rooms;
 
   constructor() { }
 
