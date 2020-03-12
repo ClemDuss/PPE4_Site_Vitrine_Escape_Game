90fc25422eb3bde24ff25e410328a030827f7355
ClemDuss <dussoll.clem@gmail.com>
2020-03-12 13:03:48 +0100

    pictures done

--- src/app/view/home/pictures/pictures.component.ts
+++ src/app/view/home/pictures/pictures.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+const picturesFolder: string = './../../../../assets/images/pictures/UserPics/';
+
+const pics: string[] = [
+  'eg1.jpg',
+  'eg2.jpg',
+  'eg3.jpg',
+  'eg4.jpg',
+  'eg5.jpg',
+  'eg6.jpg'
+]
+
 @Component({
   selector: 'app-pictures',
   templateUrl: './pictures.component.html',
@@ -7,6 +18,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PicturesComponent implements OnInit {
   public title: string = 'Photos';
+
+  public picsFolder: string = picturesFolder;
+  public pictures: string[] = pics;
 
   constructor() { }
 
